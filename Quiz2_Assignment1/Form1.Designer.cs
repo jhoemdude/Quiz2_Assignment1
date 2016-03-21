01e5c1bbee76cb4e1364e761c2a7a93c491b5a67
Princess Joem Perida <princessjoem.perida@jmc.edu.ph>
2016-03-21 12:02:58 +0800

    Finals Quiz2/Assignment1

---
+++ Quiz2_Assignment1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Quiz2_Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.User = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Log In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(108, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // User
+            // 
+            this.User.AutoSize = true;
+            this.User.Location = new System.Drawing.Point(13, 34);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(29, 13);
+            this.User.TabIndex = 4;
+            this.User.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "User Type";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label User;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Quiz2_Assignment1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Quiz2_Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+            private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.textBox1.Text) | string.IsNullOrEmpty(this.textBox2.Text))
+            {
+                MessageBox.Show("provide User Name and Password");
+            }
+ 
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Select User Type");
+            }
+            
+ 
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=pc101;Initial Catalog=SMS;User ID=sa;Password=mike";
+            conn.Open();
+ 
+            string UserName = textBox1.Text;
+            string Password = textBox2.Text;
+            string UserType = textBox3.Text;
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tbluser WHERE username = '" + textBox1.Text + "' and usertype = '" + textBox3.Text + "' and mypassword = '" + textBox2.Text + "'", conn);
+ 
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+ 
+            System.Data.SqlClient.SqlDataReader dr = null;
+            dr = cmd.ExecuteReader();
+         
+            
+                if (this.textBox3.Text == dr["UserType"].ToString() & this.textBox1.Text == dr["UserName"].ToString() & this.textBox2.Text == dr["mypassword"].ToString() & this.textBox3.Text == "Data Entry Clerk")
+                 {
+                     MessageBox.Show("*** Login Successful ***");
+                     Form1 f = new Form1();
+                     f.Show();
+                    // f.CreateUserAccountToolStripMenuItem.Enabled = false;
+                     this.Hide();
+                 }
+
+                else if (this.textBox3.Text == dr["UserType"].ToString() & this.textBox1.Text == dr["UserName"].ToString() & this.textBox2.Text == dr["mypassword"].ToString())
+                 {
+                     MessageBox.Show("*** Login Successful ***");
+                     Form1 g = new Form1();
+                     g.Show();
+                     this.Hide();
+                 }
+ 
+                 else
+                 {
+                    MessageBox.Show("Invalid UserName or Password", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                     MessageBox.Show("Access Denied!!");
+                   
+                 }          
+            
+        }
+
+            private void button1_Click_1(object sender, EventArgs e)
+            {
+
+            }
+        }
+    }
+
